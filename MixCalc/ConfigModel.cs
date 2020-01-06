1568526924be504855ef91e602794cf1d4172dea
Roy Vegard Ovesen <rovo@equinor.com>
2020-01-06 13:24:42 +0100

    :construction: Copy and adapt code from PhaseOptDcs

---
+++ MixCalc/ConfigModel.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace PhaseOptDcs
+{
+    [XmlRoot("configuration")]
+    public class ConfigModel
+    {
+        public enum PressureUnit : int
+        {
+            barg = 0,
+            bara = 1,
+        }
+
+        public enum TemperatureUnit : int
+        {
+            C = 0,
+            K = 1,
+        }
+
+        [XmlElement]
+        public string OpcUrl { get; set; }
+        [XmlElement]
+        public string OpcUser { get; set; }
+        [XmlElement]
+        public string OpcPassword { get; set; }
+
+        public static ConfigModel ReadConfig(string file)
+        {
+            XmlReaderSettings readerSettings = new XmlReaderSettings
+            {
+                IgnoreComments = true,
+                IgnoreProcessingInstructions = true,
+                IgnoreWhitespace = true
+            };
+
+            XmlReader configFileReader = XmlReader.Create(file, readerSettings);
+            XmlSerializer configSerializer = new XmlSerializer(typeof(ConfigModel));
+            ConfigModel result = (ConfigModel)configSerializer.Deserialize(configFileReader);
+            configFileReader.Close();
+
+            return result;
+        }
+    }
+
+    public class CompositionList
+    {
+        public CompositionList() { Item = new List<Component>(); }
+        [XmlElement("Component")]
+        public List<Component> Item { get; }
+
+        public double[] GetValues()
+        {
+            List<double> vs = new List<double>();
+
+            foreach (var component in Item)
+            {
+                vs.Add(component.Value);
+            }
+
+            return vs.ToArray();
+        }
+
+        public double[] GetScaledValues()
+        {
+            List<double> vs = new List<double>();
+
+            foreach (var component in Item)
+            {
+                vs.Add(component.GetScaledValue());
+            }
+
+            return vs.ToArray();
+        }
+
+        public Int32[] GetIds()
+        {
+            List<Int32> vs = new List<Int32>();
+
+            foreach (var component in Item)
+            {
+                vs.Add(component.Id);
+            }
+
+            return vs.ToArray();
+        }
+    }
+
+    public class Component
+    {
+        [XmlAttribute]
+        public string Name { get; set; }
+        [XmlAttribute]
+        public Int32 Id { get; set; }
+        [XmlAttribute]
+        public string Tag { get; set; }
+        [XmlAttribute]
+        public double ScaleFactor { get; set; }
+        [XmlIgnore]
+        public double Value { get; set; }
+
+        public double GetScaledValue()
+        {
+            return Value * ScaleFactor;
+        }
+    }
+
+    public class Measurement
+    {
+        [XmlAttribute]
+        public string Name { get; set; }
+        [XmlAttribute]
+        public string Tag { get; set; }
+        [XmlAttribute]
+        public string Type { get; set; }
+
+        [XmlIgnore]
+        public double Value { get; set; }
+    }
+
+    public class PressureMeasurement : Measurement
+    {
+        [XmlAttribute]
+        public ConfigModel.PressureUnit Unit { get; set; }
+
+        public double GetUMRConverted()
+        {
+            // Convert from Unit to bar absolute
+            const double stdAtm = 1.01325;
+            double result = 0.0;
+            switch (Unit)
+            {
+                case ConfigModel.PressureUnit.barg:
+                    result = (Value + stdAtm);
+                    break;
+                case ConfigModel.PressureUnit.bara:
+                    result = Value;
+                    break;
+                default:
+                    break;
+            }
+
+            return (result);
+        }
+
+        public double GetUnitConverted()
+        {
+            // Convert from bar absolute to Unit
+            const double stdAtm = 1.01325;
+            double result = 0.0;
+            switch (Unit)
+            {
+                case ConfigModel.PressureUnit.barg:
+                    result = (Value - stdAtm);
+                    break;
+                case ConfigModel.PressureUnit.bara:
+                    result = Value;
+                    break;
+                default:
+                    break;
+            }
+
+            return (result);
+        }
+    }
+
+    public class TemperatureMeasurement : Measurement
+    {
+        [XmlAttribute]
+        public ConfigModel.TemperatureUnit Unit { get; set; }
+
+        public double GetUMRConverted()
+        {
+            // Convert from Unit to K
+            const double zeroCelsius = 273.15;
+            double result = 0.0;
+            switch (Unit)
+            {
+                case ConfigModel.TemperatureUnit.C:
+                    result = Value + zeroCelsius;
+                    break;
+                case ConfigModel.TemperatureUnit.K:
+                    result = Value;
+                    break;
+                default:
+                    break;
+            }
+
+            return (result);
+        }
+
+        public double GetUnitConverted()
+        {
+            // Convert from K to Unit
+            const double zeroCelsius = 273.15;
+            double result = 0.0;
+            switch (Unit)
+            {
+                case ConfigModel.TemperatureUnit.C:
+                    result = Value - zeroCelsius;
+                    break;
+                case ConfigModel.TemperatureUnit.K:
+                    result = Value;
+                    break;
+                default:
+                    break;
+            }
+
+            return (result);
+        }
+    }
+}
---
+++ MixCalc/MixCalcService.cs
@@ -0,0 +1,143 @@
+﻿using Opc.Ua;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace PhaseOptDcs
+{
+    class PhaseOptDcsService : IDisposable
+    {
+        private readonly Timer timer;
+        private readonly ConfigModel config;
+        private readonly OpcClient opcClient;
+        private static readonly NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+        private readonly object WorkerLock = new object();
+
+        public PhaseOptDcsService()
+        {
+            logger.Info("Initializing PhaseOptDcsService.");
+            string ConfigFile = AppDomain.CurrentDomain.BaseDirectory.ToString(CultureInfo.InvariantCulture) + "PhaseOptDcs.config";
+            logger.Debug(CultureInfo.InvariantCulture, "Reading configuration from {0}.", ConfigFile);
+            try
+            {
+                config = ConfigModel.ReadConfig(ConfigFile);
+            }
+            catch (Exception e)
+            {
+                logger.Fatal(e, "Failed to read configuration.");
+                throw;
+            }
+
+            timer = new Timer(20_000.0) { AutoReset = true, SynchronizingObject = null };
+            timer.Elapsed += Worker;
+
+            opcClient = new OpcClient(config.OpcUrl, config.OpcUser, config.OpcPassword);
+        }
+
+        public void Dispose()
+        {
+            opcClient.Dispose();
+            timer.Dispose();
+        }
+
+        public async void Start()
+        {
+            logger.Info("Starting service.");
+            await opcClient.Connect().ConfigureAwait(false);
+            timer.Start();
+        }
+
+        private void Worker(object sender, ElapsedEventArgs ea)
+        {
+            lock (WorkerLock)
+            {
+                ReadFromOPC();
+                WriteToOPC();
+            }
+        }
+
+        private void ReadFromOPC()
+        {
+            NodeIdCollection nodes = new NodeIdCollection();
+            List<Type> types = new List<Type>();
+            List<object> result = new List<object>();
+            List<ServiceResult> errors = new List<ServiceResult>();
+
+            // Make a list of all the OPC item that we want to read
+
+            foreach (var item in nodes)
+            {
+                logger.Debug(CultureInfo.InvariantCulture, "Item to read: \"{0}\"", item.ToString());
+            }
+
+            // Read all of the inputs
+            try
+            {
+                opcClient.OpcSession.ReadValues(nodes, types, out result, out errors);
+            }
+            catch (Exception e)
+            {
+                logger.Error(e, "Error reading values from OPC.");
+            }
+
+            for (int n = 0; n < result.Count; n++)
+            {
+                logger.Debug(CultureInfo.InvariantCulture, "Item: \"{0}\" Value: \"{1}\" Status: \"{2}\"",
+                    nodes[n].ToString(), result[n], errors[n].StatusCode.ToString());
+            }
+
+        }
+
+        private void ProcessStreams()
+        {
+        }
+
+        private void WriteToOPC()
+        {
+            // Make a list of all the OPC item that we want to write
+            WriteValueCollection wvc = new WriteValueCollection();
+
+            foreach (var item in wvc)
+            {
+                logger.Debug(CultureInfo.InvariantCulture, "Item to write: \"{0}\" Value: {1}",
+                    item.NodeId.ToString(),
+                    item.Value.Value);
+            }
+
+            try
+            {
+                opcClient.OpcSession.Write(null, wvc, out StatusCodeCollection results, out DiagnosticInfoCollection diagnosticInfos);
+
+                for (int i = 0; i < results.Count; i++)
+                {
+                    if (results[i].Code != 0)
+                    {
+                        logger.Error(CultureInfo.InvariantCulture, "Write result: \"{0}\" Tag: \"{1}\" Value: \"{2}\" Type: \"{3}\"",
+                            results[i].ToString(), wvc[i].NodeId, wvc[i].Value.Value, wvc[i].Value.Value.GetType().ToString());
+                    }
+
+                }
+            }
+            catch (Exception e)
+            {
+                logger.Error(e, "Error writing OPC items");
+            }
+        }
+
+        public void Stop()
+        {
+            logger.Info("Stop service command received.");
+            timer.Stop();
+            logger.Info("Waiting for current worker.");
+            lock (WorkerLock)
+            {
+                logger.Info("Worker is done.");
+                logger.Info("Disconnecting from OPC server.");
+                opcClient.DisConnect();
+            }
+            logger.Info("Stopping service.");
+        }
+    }
+}
